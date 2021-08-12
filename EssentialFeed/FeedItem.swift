cadfe1375af07d47b63a434b6505266ed2c9e19e
zip520123 <zip520123@yahoo.com.tw>
2021-08-12 16:40:11 +0100

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by zip520123 on 12/08/2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
