b5bab6a6f5dd805b80985cf77048066c6662335f
zip520123 <zip520123@yahoo.com.tw>
2021-09-11 14:46:31 +0100

    Save command requests cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,12 +6,23 @@
 //
 
 import XCTest
-struct FeedStore {
+import EssentialFeed
+class FeedStore {
     var deleteCachedFeedCallCount = 0
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 class LocalFeedLoader {
-    init(store: FeedStore) {}
+    private let store: FeedStore
+    init(store: FeedStore) {
+        self.store = store
+    }
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
+    }
 }
+
 class CacheFeedUseCaseTests: XCTestCase {
 
     func test_init_doesNotDeleteCacheUponCreation() {
@@ -19,5 +30,21 @@ class CacheFeedUseCaseTests: XCTestCase {
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount , 0)
     }
-
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helper
+    func uniqueItem() -> FeedItem {
+        FeedItem(id: UUID(), description: nil, location: nil, imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        URL(string: "http://a-url.com")!
+    }
 }
