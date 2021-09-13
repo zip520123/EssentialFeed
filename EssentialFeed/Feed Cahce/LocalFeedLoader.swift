91ab9d488fac7bea3d19fe976dd1157762c2d4df
zip520123 <zip520123@yahoo.com.tw>
2021-09-13 15:05:14 +0100

    Move LocalFeedLoader and FeedStore to its own file in production

---
+++ EssentialFeed/Feed Cahce/LocalFeedLoader.swift
@@ -0,0 +1,44 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by zip520123 on 13/09/2021.
+//
+
+import Foundation
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?)-> Void
+    typealias InsertionCompletion = (Error?)-> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
+
+
+public final class LocalFeedLoader {
+    private let store: FeedStore
+    private let currentDate: ()->Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    public func save(_ items: [FeedItem], completion: @escaping (Error?)->Void = {_ in}) {
+        store.deleteCachedFeed {[weak self] error in
+            guard let self = self else {return}
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items, with: completion)
+            }
+        }
+    }
+    private func cache(_ items: [FeedItem], with completion: @escaping (Error?)->Void) {
+        store.insert(items, timestamp: currentDate(), completion: { [weak self] error in
+            if self != nil {
+                completion(error)
+            }
+        })
+    }
+}
--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -7,40 +7,6 @@
 
 import XCTest
 import EssentialFeed
-protocol FeedStore {
-    typealias DeletionCompletion = (Error?)-> Void
-    typealias InsertionCompletion = (Error?)-> Void
-    
-    func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
-}
-
-
-class LocalFeedLoader {
-    private let store: FeedStore
-    private let currentDate: ()->Date
-    init(store: FeedStore, currentDate: @escaping () -> Date) {
-        self.store = store
-        self.currentDate = currentDate
-    }
-    func save(_ items: [FeedItem], completion: @escaping (Error?)->Void = {_ in}) {
-        store.deleteCachedFeed {[weak self] error in
-            guard let self = self else {return}
-            if let cacheDeletionError = error {
-                completion(cacheDeletionError)
-            } else {
-                self.cache(items, with: completion)
-            }
-        }
-    }
-    private func cache(_ items: [FeedItem], with completion: @escaping (Error?)->Void) {
-        store.insert(items, timestamp: currentDate(), completion: { [weak self] error in
-            if self != nil {
-                completion(error)
-            }
-        })
-    }
-}
 
 class CacheFeedUseCaseTests: XCTestCase {
 
