5dded59d6c2501ea1003c40501f04e7a0e2f566d
zip520123 <zip520123@yahoo.com.tw>
2021-09-13 15:06:30 +0100

    Move FeedStore to its own file

---
+++ EssentialFeed/Feed Cahce/FeedStore.swift
@@ -0,0 +1,15 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by zip520123 on 13/09/2021.
+//
+
+import Foundation
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?)-> Void
+    typealias InsertionCompletion = (Error?)-> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
