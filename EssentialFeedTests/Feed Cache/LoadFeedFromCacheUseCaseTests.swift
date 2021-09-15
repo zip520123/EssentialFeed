a0f789214beab14ac0a37c0133e34810db137bd1
zip520123 <zip520123@yahoo.com.tw>
2021-09-15 19:00:55 +0100

    Load command delivers no images on more than seven days old cache

--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -63,6 +63,19 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         })
         
     }
+    
+    func test_load_deliversNoImagesOnMoreThenSevenDaysOldCache() {
+        
+        let feed = uniqueImageFeed()
+        let fixedCurrentDate = Date()
+        let moreThenSevendDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: -1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: feed.local, timestamp: moreThenSevendDaysOldTimestamp)
+        })
+        
+    }
 
     // MARK: - Helper
     
