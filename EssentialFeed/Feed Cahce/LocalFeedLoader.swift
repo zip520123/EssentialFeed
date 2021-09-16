4e685e62a02696e448d8c6bc40cf9d043bf38e12
zip520123 <zip520123@yahoo.com.tw>
2021-09-16 18:59:01 +0100

    Make `localFeedLoader` conform to `FeedLoader`

--- EssentialFeed/Feed Cahce/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cahce/LocalFeedLoader.swift
@@ -49,7 +49,7 @@ extension LocalFeedLoader {
     }
 }
 
-extension LocalFeedLoader {
+extension LocalFeedLoader: FeedLoader {
     public func load(completion: @escaping (LoadResult)->Void ) {
         store.retrieve(completion: { [weak self] result in
             guard let self = self else {return }
