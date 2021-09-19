7a05d27e4292525de99f7c260decf8031346a6a0
zip520123 <zip520123@yahoo.com.tw>
2021-09-19 17:00:46 +0100

    Delivers error on insertion error with invalid store url

--- EssentialFeedTests/Feed Cache/CodeableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodeableFeedStoreTests.swift
@@ -61,10 +61,14 @@ class CodableFeedStore {
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
         let encoder = JSONEncoder()
         let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try! encoded.write(to: storeURL)
+        do {
+            let encoded = try encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
         
-        completion(nil)
     }
 }
 
@@ -135,6 +139,17 @@ class CodeableFeedStoreTests: XCTestCase {
         
         expect(sut, toRetrieve: .found(latestFeed, latestTimeStamp))
         
+    }
+    
+    func test_insert_deliversErrorOnInsertionError() {
+        let invalidStoreURL = URL(string: "invalid://store-url")
+        let sut = makeSUT(storeURL: invalidStoreURL)
+        let feed = uniqueImageFeed().local
+        let timestamp = Date()
+        
+        let insertionError = insert(sut, feed, timestamp)
+        
+        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
     }
     
     // - MARK: Helpers
