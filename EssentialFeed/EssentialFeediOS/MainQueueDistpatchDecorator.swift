560797199b21d5d7f6230a6d66da80529a9dc846
zip520123 <zip520123@yahoo.com.tw>
2022-01-01 10:32:57 +0000

    Move `MainQueueDispatchDecorator` to separate file

---
+++ EssentialFeed/EssentialFeediOS/MainQueueDistpatchDecorator.swift
@@ -0,0 +1,43 @@
+//
+//  MainQueueDistpatchDecorator.swift
+//  EssentialFeediOS
+//
+//  Created by zip520123 on 01/01/2022.
+//
+
+import EssentialFeed
+import Foundation
+
+final class MainQueueDispatchDecorator<T> {
+    
+    private let decoratee: T
+    internal init(decoratee: T) {
+        self.decoratee = decoratee
+    }
+    
+    func dispatch(completion: @escaping ()-> Void) {
+        guard Thread.isMainThread else {
+            DispatchQueue.main.async { completion() }
+            return
+        }
+        completion()
+    }
+}
+
+extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] (result) in
+            self?.dispatch { completion(result) }
+        }
+    }
+}
+
+extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        decoratee.loadImageData(from: url) { [weak self] (result) in
+            self?.dispatch {
+                completion(result)
+            }
+        }
+    }
+}
