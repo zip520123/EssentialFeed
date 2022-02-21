4c3397ab7726ae5ad7a77442271a9304c96af157
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-02-21 23:59:39 +0000

    test_loadImageData

--- EssentialFeed/EssentialFeedTests/Feed API/LoadImageFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadImageFromRemoteUseCaseTests.swift
@@ -2,6 +2,14 @@ import XCTest
 import EssentialFeed
 
 class RemoteImageDataLoader {
+    let client: HTTPClient
+    init(client: HTTPClient) {
+        self.client = client
+    }
+
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+        client.get(from: url, completion: {_ in})
+    }
 
 }
 
@@ -12,14 +20,25 @@ class LoadImageFromRemoteUseCaseTests: XCTestCase {
 
     }
 
+    func test_loadImageData() {
+        let (client, sut) = makeSUT()
+        let url = anyURL()
+        sut.loadImageData(from: url,completion: {_ in})
+        XCTAssertEqual(client.requests, [url])
+    }
+
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (HTTPClientSpy, RemoteImageDataLoader) {
         let spy = HTTPClientSpy()
-        let sut = RemoteImageDataLoader()
+        let sut = RemoteImageDataLoader(client: spy)
         trackForMemoryLeaks(spy, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (spy, sut)
     }
-    private class HTTPClientSpy {
+    private class HTTPClientSpy: HTTPClient {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
+            requests.append(url)
+        }
+
         var requests = [URL]()
     }
 
