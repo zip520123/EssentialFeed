ae02fc1cd633ea4148eb7e86c3c0f05ba4f062d0
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-02-23 22:23:21 +0000

    Remove default URLSessionHTTPClient initializer parameter

--- EssentialFeed/EssentialFeedTests/Feed API/URLHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLHTTPClientTests.swift
@@ -10,17 +10,10 @@ import EssentialFeed
 
 
 class URLHTTPClientTests: XCTestCase {
-    class override func setUp() {
-        
-    }
-    override func setUp() {
-        super.setUp()
-        URLProtocolStub.startInterceptingRequests()
-    }
-    
+
     override func tearDown() {
         super.tearDown()
-        URLProtocolStub.stopInterceptingRequests()
+        URLProtocolStub.removeStub() 
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
@@ -91,7 +84,11 @@ class URLHTTPClientTests: XCTestCase {
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
-        let sut = URLSessionHTTPClient()
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+
+        let sut = URLSessionHTTPClient(session: session)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
@@ -172,12 +169,7 @@ class URLHTTPClientTests: XCTestCase {
             stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
         }
         
-        static func startInterceptingRequests() {
-            URLProtocol.registerClass(URLProtocolStub.self)
-        }
-        
-        static func stopInterceptingRequests() {
-            URLProtocol.unregisterClass(URLProtocolStub.self)
+        static func removeStub() {
             stub = nil
         }
         
