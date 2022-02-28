46b0d59aa6510b465b67f42c457a1f5d945b2cd9
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-02-28 10:57:20 +0000

    Move RemoteFeedImageDataLoader to production

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteImageDataLoader.swift
@@ -0,0 +1,48 @@
+
+public final class RemoteImageDataLoader {
+    let client: HTTPClient
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+
+    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
+        var httpClientTask: HTTPClientTask?
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+        init(completion: @escaping ((FeedImageDataLoader.Result) -> Void)) {
+            self.completion = completion
+        }
+
+        func cancel() {
+            httpClientTask?.cancel()
+            completion = nil
+        }
+
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+
+    }
+
+    @discardableResult
+    public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        let task = HTTPClientTaskWrapper(completion: completion)
+        task.httpClientTask = client.get(from: url, completion: { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success((data, res)):
+                if res.statusCode == 200 && !data.isEmpty{
+                    task.complete(with: .success(data))
+                } else {
+                    task.complete(with: .failure(Error.invalidData))
+                }
+            case let .failure(error):
+                task.complete(with: .failure(error))
+            }
+        })
+        return task
+    }
+
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+}
--- EssentialFeed/EssentialFeedTests/Feed API/LoadImageFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadImageFromRemoteUseCaseTests.swift
@@ -1,54 +1,6 @@
 import XCTest
 import EssentialFeed
 
-class RemoteImageDataLoader {
-    let client: HTTPClient
-    init(client: HTTPClient) {
-        self.client = client
-    }
-
-    private final class HTTPClientTaskWrapper: FeedImageDataLoaderTask {
-        var httpClientTask: HTTPClientTask?
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        init(completion: @escaping ((FeedImageDataLoader.Result) -> Void)) {
-            self.completion = completion
-        }
-
-        func cancel() {
-            httpClientTask?.cancel()
-            completion = nil
-        }
-
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
-
-    }
-
-    @discardableResult
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = HTTPClientTaskWrapper(completion: completion)
-        task.httpClientTask = client.get(from: url, completion: { [weak self] result in
-            guard self != nil else { return }
-            switch result {
-            case let .success((data, res)):
-                if res.statusCode == 200 && !data.isEmpty{
-                    task.complete(with: .success(data))
-                } else {
-                    task.complete(with: .failure(Error.invalidData))
-                }
-            case let .failure(error):
-                task.complete(with: .failure(error))
-            }
-        })
-        return task
-    }
-
-    public enum Error: Swift.Error {
-        case invalidData
-    }
-}
-
 class LoadImageFromRemoteUseCaseTests: XCTestCase {
     func test_init_doesNotPerformAnyURLRequest() {
         let (client, _) = makeSUT()
