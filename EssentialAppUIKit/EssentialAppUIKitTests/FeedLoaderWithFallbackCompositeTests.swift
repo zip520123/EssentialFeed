f313b97f17060244a6fc651a9cded115eba03f9b
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-03-15 21:07:42 +0000

    `FeedLoaderWithFailbackComposite.load` delivers fallback feed on primary loader failure

--- EssentialAppUIKit/EssentialAppUIKitTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialAppUIKit/EssentialAppUIKitTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -6,7 +6,15 @@ class FeedLoaderWithFallbackComposite: FeedLoader {
     let fallback: FeedLoader
 
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load(completion: completion)
+
+        primary.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+            case .failure:
+                self?.fallback.load(completion: completion)
+            }
+        }
     }
 
     init(primary: FeedLoader, fallback: FeedLoader) {
@@ -29,6 +37,27 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
             case let .success(receivedFeed):
 
                 XCTAssertEqual(receivedFeed, primaryFeed)
+            case .failure:
+                XCTFail("Expected successful load, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+
+    }
+
+    func test_load_deliversPrimaryFeedOnPrimaryLoaderFailure() {
+
+        let fallbackFeed = uniqueFeed()
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.load { result in
+            switch result {
+            case let .success(receivedFeed):
+
+                XCTAssertEqual(receivedFeed, fallbackFeed)
             case .failure:
                 XCTFail("Expected successful load, got \(result) instead")
             }
