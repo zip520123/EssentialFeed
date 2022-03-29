3f1deef0eb5bdb68c237bccd1cfe61ad7b0ba961
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-03-29 13:06:16 +0100

    Improve coverage by using identifiers for finding feed image cells

--- EssentialAppUIKit/EssentialAppUIKit/SceneDelegate.swift
+++ EssentialAppUIKit/EssentialAppUIKit/SceneDelegate.swift
@@ -3,13 +3,26 @@
 import UIKit
 import EssentialFeed
 import EssentialFeediOS
-import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let _ = (scene as? UIWindowScene) else { return }
+//        let url = URL(string: "https://ile-api.essentialdeveloper.com/essential-feed/v1/feed")!
+
+        let url = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+
+        let session = URLSession(configuration: .ephemeral)
+        let remoteClient = URLSessionHTTPClient(session: session)
+        let remoteFeedLoader = RemoteFeedLoader(url: url, client: remoteClient)
+        let remoteImageLoader = RemoteImageDataLoader(client: remoteClient)
+
+        window?.rootViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: remoteFeedLoader,
+            imageLoader: remoteImageLoader)
         
     }
 
