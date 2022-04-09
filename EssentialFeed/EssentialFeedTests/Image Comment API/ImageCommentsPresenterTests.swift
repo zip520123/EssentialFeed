e0d47c8fab01fbe1eee3e159f407a5fd4b62a82d
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-04-09 18:34:02 +0100

    Add image commnets title

---
+++ EssentialFeed/EssentialFeedTests/Image Comment API/ImageCommentsPresenterTests.swift
@@ -0,0 +1,21 @@
+//
+
+import XCTest
+import EssentialFeed
+
+class ImageCommentsPresenterTests: XCTestCase {
+
+    func test_title_isLocalized() {
+        XCTAssertEqual(ImageCommentsPresenter.title, localized("IMAGE_COMMENTS_VIEW_TITLE"))
+    }
+
+    func localized(_ key: String, _ file: StaticString = #file, _ line: UInt = #line) -> String {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized String for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+}
