727b781e6f183a414915a1172bebeabecd6c46ad
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-04-09 12:44:11 +0100

    Duplicate FeedPresenter as LoadResourcePresenter

---
+++ EssentialFeed/EssentialFeed/Share Presentation/LoadResourcePresenter.swift
@@ -0,0 +1,40 @@
+
+public class LoadResourcePresenter {
+    let feedErrorView: FeedErrorView
+    let loadingView: FeedLoadingView
+    let feedView: FeedView
+
+    public init(feedErrorView: FeedErrorView, loadingView: FeedLoadingView, feedView: FeedView) {
+        self.feedErrorView = feedErrorView
+        self.loadingView = loadingView
+        self.feedView = feedView
+    }
+    public func didStartLoadingFeed() {
+        feedErrorView.display(FeedErrorViewModel(errorMessage: nil))
+        loadingView.display(viewModel: FeedLoadingViewModel(isLoading: true))
+    }
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(viewModel: FeedViewModel(feeds: feed))
+        loadingView.display(viewModel: FeedLoadingViewModel(isLoading: false))
+    }
+    public func didFinishLoadingFeed(with error: Error) {
+        loadingView.display(viewModel: FeedLoadingViewModel(isLoading: false))
+        feedErrorView.display(FeedErrorViewModel(errorMessage: FeedPresenter.feedLoadError))
+    }
+
+
+    static var feedLoadError: String {
+        NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+                          tableName: "Feed",
+                          bundle: Bundle(for: FeedPresenter.self),
+                          comment: "Title for the feed view")
+
+    }
+
+    public static var title: String {
+        NSLocalizedString("FEED_VIEW_TITLE",
+                          tableName: "Feed",
+                          bundle: Bundle(for: FeedPresenter.self),
+                          comment: "Title for the feed view")
+    }
+}
---
+++ EssentialFeed/EssentialFeedTests/Share Presentation/LoadResourcePresenterTests.swift
@@ -0,0 +1,72 @@
+//
+
+import XCTest
+import EssentialFeed
+
+class LoadResourcePresenterTests: XCTestCase {
+
+    func test_init_doesNotSendMessageToView() {
+        let (_, view) = makeSUT()
+
+        XCTAssertTrue(view.msg.isEmpty)
+    }
+
+    func test_title_isLocalized() {
+        XCTAssertEqual(LoadResourcePresenter.title, localized("FEED_VIEW_TITLE"))
+    }
+
+    func test_didStartLoadingFeed_displaysNoErrorMessageAndStartLoading() {
+        let (sut, view) = makeSUT()
+
+        sut.didStartLoadingFeed()
+
+        XCTAssertEqual(view.msg, [
+            .error(msg: nil),
+            .loading(isLoading: true)
+        ])
+
+    }
+
+    func test_didFinishLoadingFeed_displaysFeedAndStopsLoading() {
+        let (sut, view) = makeSUT()
+        let feed = uniqueImageFeed().models
+        sut.didFinishLoadingFeed(with: feed)
+
+        XCTAssertEqual(view.msg, [
+            .display(feed: feed),
+            .loading(isLoading: false)
+        ])
+
+    }
+
+    func test_finishedLoadingFeed_displayErrorAndStopsLoading() {
+        let (sut, view) = makeSUT()
+        let error = anyNSError()
+        sut.didFinishLoadingFeed(with: error)
+
+        XCTAssertEqual(view.msg, [
+            .error(msg: localized("FEED_VIEW_CONNECTION_ERROR") ),
+            .loading(isLoading: false)
+        ])
+    }
+
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LoadResourcePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = LoadResourcePresenter(feedErrorView: view, loadingView: view, feedView: view)
+        trackForMemoryLeaks(view, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, view)
+
+    }
+
+    func localized(_ key: String, _ file: StaticString = #file, _ line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: LoadResourcePresenter.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized String for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+
+}
