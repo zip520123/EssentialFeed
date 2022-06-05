2ada32f4817dc45cf9dd23417daed786f1ddcae3
Hsiang-Lin <zip520123@yahoo.com.tw>
2022-06-05 08:43:56 +0100

    Add AnyScheduler

--- EssentialAppUIKit/EssentialAppUIKit/CombineHelpers.swift
+++ EssentialAppUIKit/EssentialAppUIKit/CombineHelpers.swift
@@ -151,3 +151,52 @@ extension DispatchQueue {
 
     }
 }
+
+typealias AnyDispatchQueueScheduer = AnyScheduler<DispatchQueue.SchedulerTimeType, DispatchQueue.SchedulerOptions>
+
+extension AnyDispatchQueueScheduer {
+    static var immediateOnMainQueue: Self {
+        DispatchQueue.immediateWhenOnMainQueueScheduler.eraseToAnyScheduler()
+    }
+}
+
+extension Scheduler {
+    func eraseToAnyScheduler() -> AnyScheduler<SchedulerTimeType, SchedulerOptions> {
+        AnyScheduler(self)
+    }
+}
+
+struct AnyScheduler<SchedulerTimeType : Strideable, SchedulerOptions>: Scheduler where SchedulerTimeType.Stride : SchedulerTimeIntervalConvertible {
+
+
+    private let _now: () -> SchedulerTimeType
+    var now: SchedulerTimeType { _now() }
+
+    private let _minimumTolerance: ()->SchedulerTimeType.Stride
+    var minimumTolerance: SchedulerTimeType.Stride { _minimumTolerance() }
+
+
+    init<S>(_ scheduler: S) where SchedulerTimeType == S.SchedulerTimeType, SchedulerOptions == S.SchedulerOptions, S: Scheduler {
+        _now = { scheduler.now }
+        _minimumTolerance = { scheduler.minimumTolerance }
+        _schedule = scheduler.schedule(options:_:)
+        _scheduleAfter = scheduler.schedule(after:tolerance:options:_:)
+        _scheduleAfterInterval = scheduler.schedule(after:interval:tolerance:options:_:)
+    }
+
+    private let _schedule: (SchedulerOptions?, @escaping () -> Void) -> Void
+    func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
+        _schedule(options, action)
+    }
+
+    private let _scheduleAfter: (SchedulerTimeType, SchedulerTimeType.Stride, SchedulerOptions?, @escaping () -> Void) -> Void
+    func schedule(after date: SchedulerTimeType, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) {
+        _scheduleAfter(date, tolerance, options, action)
+    }
+
+    private let _scheduleAfterInterval: (SchedulerTimeType, SchedulerTimeType.Stride, SchedulerTimeType.Stride, SchedulerOptions?, @escaping () -> Void) -> Cancellable
+    func schedule(after date: SchedulerTimeType, interval: SchedulerTimeType.Stride, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) -> Cancellable {
+        _scheduleAfterInterval(date, interval, tolerance, options, action)
+    }
+
+}
